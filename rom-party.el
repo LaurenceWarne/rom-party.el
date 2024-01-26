9a97b9a3bde5dbbbb9dc72214000b24b3449b949
Laurence Warne <laurencewarne@gmail.com>
2024-01-26 19:21:39 +0000

    Add commentary

--- rom-party.el
+++ rom-party.el
@@ -1,4 +1,4 @@
-;;; rom-party.el --- Bomb Party... in Emacs -*- lexical-binding: t -*-
+;;; rom-party.el --- Bomb Party -*- lexical-binding: t -*-
 
 ;; Author: Laurence Warne
 ;; Maintainer: Laurence Warne
@@ -21,7 +21,17 @@
 
 ;;; Commentary:
 
-;; Bomb Party... in Emacs
+;; The main entry point to the package is the command `rom-party' which
+;; will start a new game.  The objective of the game is, given a two or
+;; three letter prompt, to find any word containg that prompt as a
+;; substring in the given time limit (as in https://jklm.fun/).
+
+;; The difficulty of the game can be configured by the user custom
+;; variable `rom-party-prompt-filter', see the docstring for more
+;; information.
+
+;; Note by default on first invocation, `rom-party' will download a
+;; remote index file asynchronously and so may not start straight away.
 
 ;;; Code:
 
@@ -132,7 +142,7 @@ the same Emacs session."
   :type 'boolean)
 
 (defcustom rom-party-prompt-filter
-  (lambda (_prompt words) (>= (length words) 5))
+  (lambda (_prompt words) (>= (length words) 50))
   "Function called to filter rom party prompts.
 
 It should take two arguments, the first of which is the prompt itself, and the
